f8b858cab475c9ade3c56de9f541e901d0174992
ktbg <ktboates@me.com>
2021-10-05 12:02:11 -0400

    screen boilerplate updated

---
+++ client/src/screens/Create/Create.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom'
+import { createPost } from '../../services/posts';
+import Layout from '../../components/Layout/Layout'
+
+const Create = () => {
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
+
+export default Create
---
+++ client/src/screens/Details/Details.jsx
@@ -0,0 +1,17 @@
+import { useParams, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { getPost } from '../../services/posts.js'
+import Layout from '../../components/Layout/Layout.jsx'
+// delete component
+
+
+const Details = () => {
+  
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
+
+export default Details
